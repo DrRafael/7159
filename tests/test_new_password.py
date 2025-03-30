d566238c0d5b200756fdb2c6a57c0965bd9dcba6
Aydarchik <aydar.gumarov@inbox.ru>
2025-03-30 16:41:08 +0500

    Добавление тестов

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -7,6 +7,15 @@ def test_password_characters():
     password = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
     for char in password:
         assert char in valid_characters
+def test_password_length():
+    """Тест, что длина пароля соответствует заданной"""
+    password = generate_password(10)
+    assert len(password) == 10
+def test_password_different():
+    """Тест, что два сгенерированных подряд пароля различаются"""
+    password1 = generate_password(10)
+    password2 = generate_password(10)
+    assert password1 != password2
 
 """
 Допиши еще один тест из предложенных. Или придумай свой.
